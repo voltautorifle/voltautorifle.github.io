902d4403cac9f6cc87d2712febc598ac9dad9087
voltautorifle <35408734+voltautorifle@users.noreply.github.com>
2018-02-18 01:44:58 -0700

    Update currentdate.js

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,5 +1,5 @@
 function time
-SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+SimpleDateFormat sdf = new SimpleDateFormat("DD/MM/yyyy");
 Date date = new Date();
 String date3= sdf.format(date);
 date = sdf.parse(date3);
