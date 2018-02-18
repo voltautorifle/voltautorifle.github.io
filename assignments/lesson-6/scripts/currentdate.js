16d935da3abf7ae4fd74f2d34c2f0e1ac0cf97ed
voltautorifle <35408734+voltautorifle@users.noreply.github.com>
2018-02-18 01:52:30 -0700

    Update currentdate.js

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,4 +1,22 @@
 function time
+<p id="demo"></p>
+
+<script>
+var d = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+document.getElementById("demo").innerHTML = days[d.getDay()];
+</script>
+<script>
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getDate();
+</script> 
+<script>
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getFullYear();
+</script> 
+ 
+</body>
+</html>
 SimpleDateFormat sdf = new SimpleDateFormat("DD/MM/yyyy");
 Date date = new Date();
 String date3= sdf.format(date);
