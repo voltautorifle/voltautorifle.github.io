9bfc3016ee3327d247a21dc57299927138f14a1d
voltautorifle <35408734+voltautorifle@users.noreply.github.com>
2018-02-18 02:00:12 -0700

    Update currentdate.js

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -11,12 +11,10 @@ function time
 var d = new Date();
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 document.getElementById("demo").innerHTML = days[d.getDay()];
-</script>
-<script>
+
 var d = new Date();
 document.getElementById("demo").innerHTML = d.getDate();
-</script> 
-<script>
+
 var d = new Date();
 document.getElementById("demo").innerHTML = d.getFullYear();
 </script> 
